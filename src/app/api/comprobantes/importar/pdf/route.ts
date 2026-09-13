6a84edd2839b6422b3b88178bd47a57a4a4b8bd2
Fede <fedenilomilesi@users.noreply.github.com>
2026-09-13 18:32:16 -0300

    fix: pdf-parse vercel edge bug

--- src/app/api/comprobantes/importar/pdf/route.ts
+++ src/app/api/comprobantes/importar/pdf/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from 'next/server';
-const pdfParse = require('pdf-parse');
-
 export async function POST(request: Request) {
   try {
+    const pdfParse = require('pdf-parse');
     const formData = await request.formData();
     const files = formData.getAll('files') as File[];
 
